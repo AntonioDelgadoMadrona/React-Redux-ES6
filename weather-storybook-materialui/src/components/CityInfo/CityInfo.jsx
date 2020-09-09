0ba16f46d42a4470dada3f67350a5e81b94de79d
tonidelgadomadrona <50912526+tonidelgadomadrona@users.noreply.github.com>
2020-09-09 09:48:58 +0200

    Started weather app with testing

---
+++ weather-storybook-materialui/src/components/CityInfo/CityInfo.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+
+const CityInfo = ({ city, country }) => {
+    return (
+        <div>
+            <Typography variant="h4" display="inline">{city}, </Typography>
+            <Typography variant="h5" display="inline">{country}</Typography>
+        </div>
+    )
+};
+
+CityInfo.propTypes = {
+    city: PropTypes.string.isRequired,
+    country: PropTypes.string.isRequired,
+};
+
+export default CityInfo;
